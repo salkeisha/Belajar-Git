3b00665a173906c6dacb3b1b90759f826ce3eee5
salkeisha <keishasetiadi48@gmail.com>
2025-09-11 20:35:23 +0700

    soal if else

---
+++ pertemuan2/soal.java
@@ -0,0 +1,19 @@
+package pertemuan2;
+import java.util.Scanner;
+public class soal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int angka;
+
+        angka=sc.nextInt()
+
+        if (angka % 2 == 0) {
+            System.out.println(angka+" adalah bilangan genap");
+        } else if ()
+         } else {
+            System.out.println(angka+" adalah bilangan ganjil");
+         }
+    }
+}
